20a81d9c53bcd78de091ff2c1ddbbfffe1e1371a
goku12l <150599354+goku12l@users.noreply.github.com>
2024-03-07 06:24:38 -0500

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,7 +1,7 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 local Window = redzlib:MakeWindow({
   Title = "Avalon Hub : Blox Fruits",
-  SubTitle = "by Peerless",
+  SubTitle = "by Peerlessgod",
   SaveFolder = "Avalon Hub | Blox Fruits.lua"
 })
 
@@ -12,7 +12,7 @@ Discord:AddDiscordInvite({
   Name = "Avalon Hub | Community",
   Description = "Join our discord community to receive information about the next update",
   Logo = "rbxassetid://15799458477",
-  Invite = "https://discord.gg/7aR7kNVt4g"
+  Invite = "https://discord.gg/2apFTuuzGS"
 })
 local MainFarm = Window:MakeTab({"Farm", "Home"})
 if Sea3 then
