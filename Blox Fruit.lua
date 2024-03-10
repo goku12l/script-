f098e71c20f8ff4ddc73e129e5805dcd23fcc48c
goku12l <150599354+goku12l@users.noreply.github.com>
2024-03-10 08:52:25 -0400

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -7,7 +7,7 @@ local Window = redzlib:MakeWindow({
 
 local AFKOptions = {}
 
-local Discord = Window:MakeTab("Discord", "Info")
+local Discord = Window:MakeTab("Discord", "Info") -- Changed MakeTab parameter
 Discord:AddDiscordInvite({
     Name = "Avalon Hub | Community",
     Description = "Join our discord community to receive information about the next update",
@@ -15,11 +15,11 @@ Discord:AddDiscordInvite({
     Invite = "https://discord.gg/2apFTuuzGS"
 })
 
-local Farm = Window:MakeTab("Farm", "Home") -- Changed to AddTab
+local MainFarm = Window:MakeTab("Farm", "Home") -- Changed MakeTab parameter
 if Sea3 then
-    local AutoSea = Window:MakeTab("Sea", "Waves")
-    AutoSea:AddSection("Kitsune")
-    local KILabel = AutoSea:AddParagraph("Kitsune Island : not spawn")
+    local AutoSea = Window:MakeTab("Sea", "Waves") -- Changed MakeTab parameter
+    AutoSea:AddSection("Kitsune") -- Removed unnecessary array brackets
+    local KILabel = AutoSea:AddParagraph("Kitsune Island : not spawn") -- Removed unnecessary array brackets
     AutoSea:AddToggle({
         Name = "Auto Kitsune Island",
         Callback = function(Value)
