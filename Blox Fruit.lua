5579d56e08cae6df172e14b65390f1eef2ecab4b
goku12l <150599354+goku12l@users.noreply.github.com>
2024-03-10 11:00:23 -0400

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,4 +1,4 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.lua"))()
 local Window = redzlib:MakeWindow({
     Title = "Avalon Hub : Blox Fruits",
     SubTitle = "by Peerlessgod",
@@ -16,13 +16,19 @@ Discord:AddDiscordInvite({
 })
 
 local MainFarm = Window:MakeTab({"Farm", "Home"})
+
 if Sea3 then
-  local AutoSea = Window:MakeTab({"Sea", "Waves"})
-  AutoSea:AddSection({"Kitsune"})
-  local KILabel = AutoSea:AddParagraph({"Kitsune Island : not spawn"})
-  AutoSea:AddToggle({Name = "Auto Kitsune Island",Callback = function(Value)
-    getgenv().AutoKitsuneIsland = Value;AutoKitsuneIsland()
-  end})
+    local AutoSea = Window:MakeTab({"Sea", "Waves"})
+    AutoSea:AddSection({"Kitsune"})
+    local KILabel = AutoSea:AddParagraph({"Kitsune Island : not spawn"})
+    AutoSea:AddToggle({
+        Name = "Auto Kitsune Island",
+        Callback = function(Value)
+            getgenv().AutoKitsuneIsland = Value
+            AutoKitsuneIsland()
+        end
+    })
+end
 
 local Informacoes = Window:MakeTab({"Info", "Search"})
 Informacoes:AddSection({"Player"})
