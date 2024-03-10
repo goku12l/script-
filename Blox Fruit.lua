f41d0e8814ef021841bb6590eccc87fa56424e96
goku12l <150599354+goku12l@users.noreply.github.com>
2024-03-10 08:43:43 -0400

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,16 +1,16 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.lua"))()
 local Window = redzlib:MakeWindow({
-  Title = "Avalon Hub : Blox Fruits",
-  SubTitle = "by Peerlessgod",
-  SaveFolder = "Avalon Hub | Blox Fruits.lua"
+    Title = "Avalon Hub : Blox Fruits",
+    SubTitle = "by Peerlessgod",
+    SaveFolder = "Avalon Hub | Blox Fruits.lua"
 })
 
 local AFKOptions = {}
 
 local Discord = Window:MakeTab({"Discord", "Info"})
 Discord:AddDiscordInvite({
-  Name = "Avalon Hub | Community",
-  Description = "Join our discord community to receive information about the next update",
-  Logo = "rbxassetid://15799458477",
-  Invite = "https://discord.gg/2apFTuuzGS"
+    Name = "Avalon Hub | Community",
+    Description = "Join our discord community to receive information about the next update",
+    Logo = "rbxassetid://15799458477",
+    Invite = "https://discord.gg/2apFTuuzGS"
 })
