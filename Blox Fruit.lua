2e85530c3acf742b4de9044081e370bd55ff4208
goku12l <150599354+goku12l@users.noreply.github.com>
2024-03-10 09:17:59 -0400

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,16 +1,24 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.lua"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 local Window = redzlib:MakeWindow({
-    Title = "Avalon Hub : Blox Fruits",
-    SubTitle = "by Peerlessgod",
-    SaveFolder = "Avalon Hub | Blox Fruits.lua"
+  Title = "Avalon Hub : Blox Fruits",
+  SubTitle = "by Peerlessgod",
+  SaveFolder = "Avalon Hub | Blox Fruits.lua"
 })
 
 local AFKOptions = {}
 
-local Discord = Window:MakeTab("Discord", "Info") -- You don't need to pass an array to MakeTab
+local Discord = Window:MakeTab({"Discord", "Info"})
 Discord:AddDiscordInvite({
-    Name = "Avalon Hub | Community",
-    Description = "Join our discord community to receive information about the next update",
-    Logo = "rbxassetid://15799458477",
-    Invite = "https://discord.gg/2apFTuuzGS"
+  Name = "Avalon Hub | Community",
+  Description = "Join our discord community to receive information about the next update",
+  Logo = "rbxassetid://15799458477",
+  Invite = "https://discord.gg/2apFTuuzGS"
 })
+local MainFarm = Window:MakeTab({"Farm", "Home"})
+if Sea3 then
+  local AutoSea = Window:MakeTab({"Sea", "Waves"})
+  AutoSea:AddSection({"Kitsune"})
+  local KILabel = AutoSea:AddParagraph({"Kitsune Island : not spawn"})
+  AutoSea:AddToggle({Name = "Auto Kitsune Island",Callback = function(Value)
+    getgenv().AutoKitsuneIsland = Value;AutoKitsuneIsland()
+  end})
