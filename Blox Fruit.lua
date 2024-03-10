09c2f2398bf3d42126ae2d67a241dab171ebd302
goku12l <150599354+goku12l@users.noreply.github.com>
2024-03-10 09:52:56 -0400

    Update Blox Fruit.lua

--- Blox Fruit.lua
+++ Blox Fruit.lua
@@ -1,24 +1,28 @@
-local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.lua"))()
 local Window = redzlib:MakeWindow({
-  Title = "Avalon Hub : Blox Fruits",
-  SubTitle = "by Peerlessgod",
-  SaveFolder = "Avalon Hub | Blox Fruits.lua"
+    Title = "Avalon Hub : Blox Fruits",
+    SubTitle = "by Peerlessgod",
+    SaveFolder = "Avalon Hub | Blox Fruits.lua"
 })
 
 local AFKOptions = {}
 
-local Discord = Window:MakeTab({"Discord", "Info"})
+local Discord = Window:MakeTab("Discord", "Info")
 Discord:AddDiscordInvite({
-  Name = "Avalon Hub | Community",
-  Description = "Join our discord community to receive information about the next update",
-  Logo = "rbxassetid://15799458477",
-  Invite = "https://discord.gg/2apFTuuzGS"
+    Name = "Avalon Hub | Community",
+    Description = "Join our discord community to receive information about the next update",
+    Logo = "rbxassetid://15799458477",
+    Invite = "https://discord.gg/2apFTuuzGS"
 })
-local Farm = Window:MakeTab({"Farm", "Home"})
-if Sea3 then
-  local AutoSea = Window:MakeTab({"Sea", "Waves"})
-  AutoSea:AddSection({"Kitsune"})
-  local KILabel = AutoSea:AddParagraph({"Kitsune Island : not spawn"})
-  AutoSea:AddToggle({Name = "Auto Kitsune Island",Callback = function(Value)
-    getgenv().AutoKitsuneIsland = Value;AutoKitsuneIsland()
-  end})
+
+local FarmTab = Window:MakeTab("Farm", "Home") -- Changed tab variable name to FarmTab
+local AutoSea = Window:MakeTab("Sea", "Waves")
+AutoSea:AddSection("Kitsune")
+local KILabel = AutoSea:AddParagraph("Kitsune Island : not spawn")
+AutoSea:AddToggle({
+    Name = "Auto Kitsune Island",
+    Callback = function(Value)
+        getgenv().AutoKitsuneIsland = Value
+        AutoKitsuneIsland()
+    end
+})
