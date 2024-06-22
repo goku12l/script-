3a0e2b451f8a37da6a0deffc9813194621e65882
goku12l <150599354+goku12l@users.noreply.github.com>
2024-06-22 19:27:11 -0400

    Update Clicker Simulator.lua

--- Clicker Simulator.lua
+++ Clicker Simulator.lua
@@ -1,38 +1,20 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 local Window = redzlib:MakeWindow({
-    Title = "AvalonHub [Clicker Simulator]", 
-    SubTitle = "", 
-    SaveFolder = "RedzConfig"
+  Title = "Avalon Hub : Clicker Simulator",
+  SubTitle = "by peerless",
+  SaveFolder = "Avalon Hub | Clicker Simulator.lua"
 })
 
-local Tab1 = Window:MakeTab({"Auto Farm", " you will be able to auto Farm"})
-
-local AutoFarmToggle = Tab1:AddToggle({
-    Name = "Auto Farm",
-    Description = "Start Clicking for money",
-    Default = false
+local Discord = Window:MakeTab({"Discord", "Info"})
+Discord:AddDiscordInvite({
+  Name = "redz Hub | Community",
+  Description = "Join our discord community to receive information about the next update",
+  Logo = "rbxassetid://15298567397",
+  Invite = "https://discord.gg/7aR7kNVt4g"
 })
 
--- Function to start auto-farming
-local function StartAutoFarm()
-    print("Auto Farm Started")
-    while AutoFarmToggle.Value do
-        game:GetService("ReplicatedStorage").Remotes.Clicker:FireServer()
-        print("Clicked for money")
-        wait(0.1) -- Adjust the wait time as needed
-    end
-    print("Auto Farm Stopped")
-end
+local MainFarm = Window:MakeTab({"Farm", "Home"})
 
--- Listen for changes in the toggle value
-AutoFarmToggle.OnChanged = function(value)
-    print("Toggle changed: ", value)
-    if value then
-        StartAutoFarm()
-    end
-end
-
--- Initial check if the toggle is on by default
-if AutoFarmToggle.Value then
-    StartAutoFarm()
-end
+ MainFarm:AddToggle({Name = "Auto Farm Clicker",Callback = function(Value)
+    print("works")
+  end})
