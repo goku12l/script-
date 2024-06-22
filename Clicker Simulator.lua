0ff9ce1c72140d7dd9ce6782ce1b59d03d57781f
goku12l <150599354+goku12l@users.noreply.github.com>
2024-06-22 18:08:14 -0400

    Update Clicker Simulator.lua

--- Clicker Simulator.lua
+++ Clicker Simulator.lua
@@ -13,8 +13,9 @@ local Window = redzlib:MakeWindow({
     Default = false
   })
   
- 
-  
-  Toggle1 = game:GetService("ReplicatedStorage").Remotes.Clicker:FireServer()
+  Toggle1:SetValue(true) -- Assuming this function sets the toggle to true or false
 
-  
+    -- Assuming the FireServer part needs to be wrapped in a function or a conditional
+    if Toggle1.Value then
+        game:GetService("ReplicatedStorage").Remotes.Clicker:FireServer()
+    end
