875b363f0f64508106b0d0c0f80d75dd50f1bc34
goku12l <150599354+goku12l@users.noreply.github.com>
2024-06-22 17:57:16 -0400

    Create Clicker Simulator.lua

---
+++ Clicker Simulator.lua
@@ -0,0 +1,11 @@
+local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
+local Window = redzlib:MakeWindow({
+    Title = "AvalonHub [Clicker Simulator]", 
+    SubTitle = "peerlessgod", 
+    SaveFolder = "RedzConfig"
+  })
+  
+  local Tab1 = Window:MakeTab({"Auto Farm", " you will be able to auto Farm"})
+
+
+  
