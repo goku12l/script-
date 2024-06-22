1323fc1845c1195d02d11531018d04ec47951818
goku12l <150599354+goku12l@users.noreply.github.com>
2024-06-22 18:06:26 -0400

    Update Clicker Simulator.lua

--- Clicker Simulator.lua
+++ Clicker Simulator.lua
@@ -1,11 +1,20 @@
 local redzlib = loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/RedzLibV5/main/Source.Lua"))()
 local Window = redzlib:MakeWindow({
     Title = "AvalonHub [Clicker Simulator]", 
-    SubTitle = "peerlessgod", 
+    SubTitle = "", 
     SaveFolder = "RedzConfig"
   })
   
   local Tab1 = Window:MakeTab({"Auto Farm", " you will be able to auto Farm"})
 
+  local Toggle1 = Tab1:AddToggle({
+    Name = "Auto Farm",
+    Description = "Start Clicking for money",
+    Default = false
+  })
+  
+ 
+  
+  Toggle1 = game:GetService("ReplicatedStorage").Remotes.Clicker:FireServer()
 
   
